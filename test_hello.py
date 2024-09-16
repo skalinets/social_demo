1f72319c1f5bd5067cacfc09435ce90f6989eefd
Serhii Kalinets <kalinets@gmail.com>
2024-09-16 21:22:40 +0300

    asdfsadf

--- test_hello.py
+++ test_hello.py
@@ -1,6 +1,6 @@
 import redis
 
-from main import create_status, create_user
+from main import create_status, create_user, get_status_messages
 
 
 def test_hi():
@@ -13,8 +13,10 @@ def test_redis():
     assert r.get("foo") == "bar"
 
 
+conn = redis.Redis(decode_responses=True)
+
+
 def test_create_user():
-    conn = redis.Redis()
     login = "yoba"
     name = "Yoba Yobaevich"
     create_user(conn, login, name)
@@ -22,7 +24,6 @@ def test_create_user():
 
 
 def test_create_post():
-    conn = redis.Redis()
     conn.flushdb()
     uid = create_user(conn, "fl", "dsdl")
     assert uid == 1
@@ -30,6 +31,17 @@ def test_create_post():
     message = "Hello, world!"
     message_id = create_status(conn, uid, message, **data)
     assert message_id == 1
-    posts = conn.hget("user:%s"%uid, "posts")
-    assert b'1' == posts
+    posts = conn.hget("user:%s" % uid, "posts")
+    assert "1" == posts
 
+
+def test_get_status_messages():
+    conn.flushdb()
+    uid = create_user(conn, "fl", "dsdl")
+    assert uid == 1
+    data = {}
+    message = "Hello, world!"
+    message_id = create_status(conn, uid, message, **data)
+    assert message_id == 1
+    messages = get_status_messages(conn, uid)
+    assert messages[0]["message"] == message
